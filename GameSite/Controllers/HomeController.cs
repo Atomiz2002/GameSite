168ccf46f1f5b211a100f79532c34e0f15f6776a
Atomiz <tytunipex@gmail.com>
2022-10-28 13:35:17 +0300

    Initial Commit

---
+++ GameSite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using GameSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameSite.Controllers;
+
+public class HomeController : Controller {
+
+	private readonly ILogger<HomeController> _logger;
+
+	public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+	public IActionResult Index() => View();
+
+	public IActionResult Privacy() => View();
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error() =>
+		View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+
+}
